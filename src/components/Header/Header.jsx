2b1f17ec56de3cd679a37fc6e97dfb6ab7cccf62
MECHAT Matthieu <mechat.m@icloud.com>
2024-10-25 11:18:10 +0200

    Add more test on Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import './Header.scss';
 import { getEnvVariable } from '../../utils/env';
 
 const apiKey = getEnvVariable('VITE_API_URL');
 console.log(apiKey);
 
-function Header () {
-    
-    return(
+function Header({ title = "Titre par défaut" }) {
+    const [headerTitle, setHeaderTitle] = useState(title);
 
+    const handleClick = () => {
+        setHeaderTitle("Titre modifié après clic !");
+    };
+
+    return (
         <div className="Header">
-            <h1 className="Header-title">J'ai réussi à deploy avec un push !</h1>
+            <h1 className="Header-title">{headerTitle}</h1>
+            <button onClick={handleClick}>Modifier le Titre</button>
         </div>
-    )
-
+    );
 }
 
 export default Header;
--- src/components/Header/Header.test.jsx
+++ src/components/Header/Header.test.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Header from './Header';
 
 // Mock de la fonction utilitaire
 jest.mock('../../utils/env', () => ({
-    getEnvVariable: jest.fn().mockReturnValue('https://example.com/api'), // Retournez la valeur que vous voulez
-  }));
+    getEnvVariable: jest.fn().mockReturnValue('https://example.com/api'),
+}));
 
-test('renders header title', () => {
-  render(<Header />);
-  const titleElement = screen.getByText(/Titre en H1 du Header/i);
-  expect(titleElement).toBeInTheDocument();
+test('renders header title and modifies it on button click', () => {
+    render(<Header title="Titre en H1 du Header" />);
+
+    // Vérifier que le titre est présent
+    const titleElement = screen.getByText(/Titre en H1 du Header/i);
+    expect(titleElement).toBeInTheDocument();
+
+    // Vérifier que le bouton modifie le titre
+    const buttonElement = screen.getByRole('button', { name: /Modifier le Titre/i });
+    fireEvent.click(buttonElement);
+
+    const modifiedTitleElement = screen.getByText(/Titre modifié après clic/i);
+    expect(modifiedTitleElement).toBeInTheDocument();
 });
